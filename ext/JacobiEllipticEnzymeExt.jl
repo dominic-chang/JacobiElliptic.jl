42a7135ba22797914eb24a27ae04068e98f4097e
Dominic <dchang3419@hotmail.com>
2024-03-30 16:39:51 -0400

    Add package extension for faster Enzyme gradients

--- ext/JacobiEllipticEnzymeExt.jl
+++ ext/JacobiEllipticEnzymeExt.jl
@@ -4,42 +4,92 @@ using JacobiElliptic, Enzyme
 import .EnzymeRules: forward, reverse, augmented_primal
 using .EnzymeRules
 
-function forward(func::Const{typeof(JacobiElliptic.F)}, ::Type{<:Duplicated}, ϕ::Type{<:Duplicated}, k::Type{<:Const}) 
-    println("forward call")
-    m = k.val
-    dret = JacobiElliptic.E(ϕ.val, m) / (2 * m * (1 - m)) -
-    JacobiElliptic.F(ϕ.val, m) / 2 / m -
-    sin(2*ϕ.val) / (4 * (1 - m) * √(1 - m * sin(ϕ.val)^2)) 
-
-    ret = func.val(ϕ.val, m)
-    return Duplicated(ret, dret* ϕ.dval)
+#----------------------------------------------------------------------------------------
+# Elliptic F(ϕ, m)
+#----------------------------------------------------------------------------------------
+function ∂F_∂m(ϕ, m)
+    return JacobiElliptic.E(ϕ, m) / (2 * m * (1 - m)) -
+    JacobiElliptic.F(ϕ, m) / 2 / m -
+    sin(2*ϕ) / (4 * (1 - m) * √(1 - m * sin(ϕ)^2)) 
 end
 
-function augmented_primal(config::ConfigWidth{1}, func::Const{typeof(sqrt)}, ::Type{<:Active}, x::Active{<:Complex{T}}) where {T<:Real}
+function ∂F_∂ϕ(ϕ, m)
+    return 1 / √(1 - m*sin(ϕ)^2)
+end
+
+function forward(
+    ::Const{typeof(JacobiElliptic.F)},
+    ::Type,
+    ϕ::Const,
+    m::Duplicated
+) 
+    println("In custom forward rule.")
+
+    return ∂F_∂m(ϕ.val, m.val)*m.dval
+end
+
+function forward(
+    ::Const{typeof(JacobiElliptic.F)},
+    ::Type,
+    ϕ::Duplicated,
+    m::Const
+) 
+    println("In custom forward rule.")
+
+    return ∂F_∂ϕ(ϕ.val, m.val)*ϕ.dval
+end
+
+function forward(
+    ::Const{typeof(JacobiElliptic.F)},
+    ::Type,
+    ϕ::Duplicated,
+    m::Duplicated
+) 
+    println("In custom forward rule.")
+
+    return ∂F_∂m(ϕ.val, m.val)*m.dval + ∂F_∂ϕ(ϕ.val, m.val)*ϕ.dval
+end
+
+function augmented_primal(
+    config,
+    func::Const{typeof(JacobiElliptic.F)},
+    ::Type{<:Active},
+    tape,
+    ϕ,
+    m
+ ) 
     println("In custom augmented primal rule.")
-    if needs_primal(config)
-        primal = func.val(x.val)
-    else
-        primal = nothing
-    end
-
     # Save x in tape if x will be overwritten
-    if overwritten(config)[2]
-        tape = copy(x.val)
-    else
-        tape = nothing
-    end
+    primal = EnzymeRules.needs_primal(config) ? func(ϕ.val, m.val) : nothing
 
-    # Return an AugmentedReturn object with shadow = nothing
-    return AugmentedReturn(primal, nothing, tape)
+    return EnzymeRules.AugmentedReturn(primal, nothing, tape)
 end
 
-function reverse(config::ConfigWidth{1}, ::Const{typeof(sqrt)}, dret::Active, tape, x::Active{<:Complex{T}}) where {T<:Real}  
+
+function reverse(config::ConfigWidth{N}, ::Const{JacobiElliptic.F}, dret::Active, tape, ϕ::Const, m::Active) where {N}  
     println("In custom reverse rule.")
     # retrieve x value, either from original x or from tape if x may have been overwritten.
-    xval = overwritten(config)[2] ? tape : x.val
-    dx = inv(2*sqrt(xval))' * dret.val
-    return (dx, )
+    mval = m.val
+    dm = ∂F_∂m(ϕ.val, mval) * dret.val
+    return (dm, )
+end
+
+function reverse(config::ConfigWidth{N}, ::Const{JacobiElliptic.F}, dret::Active, tape, ϕ::Active, m::Const) where {N}  
+    println("In custom reverse rule.")
+    # retrieve x value, either from original x or from tape if x may have been overwritten.
+    ϕval = EnzymeRules.overwritten(config)[2] ? tape : ϕ.val
+    dϕ = ∂F_∂ϕ(ϕval, m.val) * dret.val
+    return (dϕ, )
+end
+
+function reverse(config::ConfigWidth{N}, ::Const{JacobiElliptic.F}, dret::Active, tape, ϕ::Active, m::Active) where {N}  
+    println("In custom reverse rule.")
+    # retrieve x value, either from original x or from tape if x may have been overwritten.
+    ϕval = ϕ.val
+    mval = m.val
+    dm = ∂F_∂m(ϕval, mval) * dret.val
+    dϕ = ∂F_∂ϕ(ϕval, mval) * dret.val
+    return (dϕ, dm)
 end
 
 
