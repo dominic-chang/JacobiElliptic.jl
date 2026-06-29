fdd941b3657eae7fd39a5fab6252ee23c279a56e
Julian Mühlenhoff <julian.muehlenhoff@tu-ilmenau.de>
2026-06-29 03:40:33 +0200

    added test cases for phi = 0.5 and m = 1.0

--- test/autodiff.jl
+++ test/autodiff.jl
@@ -185,6 +185,12 @@ end
                 for Tret in (Const, Active), Tϕ in (Const, Active), Tm in (Const, Active)
                     test_reverse(alg.F, Tret, (ϕ, Tϕ), (m, Tm); atol = enzyme_atol)
                 end
+
+                # Special case m = 1:
+                ϕ = 0.5; m = 1.0
+                @test ForwardDiff.derivative(x -> _F(ϕ + x, m), 0.0) ≈ 1.139493927324549 atol = 1e-9 # Number retrieved from ForwardDiff and checked against finite diff
+                @test ForwardDiff.derivative(x -> _F(ϕ, m + x), 0.0) ≈ 0.025067566595210033 atol = 1e-9 # Number retrieved from ForwardDiff and checked against finite diff
+                @test ForwardDiff.derivative(x -> _F(ϕ + x, m + x), 0.0) ≈ 1.164561493919759 atol = 1e-9 # Number retrieved from ForwardDiff and checked against finite diff
             end
 
             @testset "Incomplete E" begin
